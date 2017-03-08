4a8e28899a91e157324041d3db093ba633df7b7a
Maximo Piva <maximo.piva@gmail.com>
2017-03-08 15:26:12 -0300

    Add project files.

---
+++ Goova.Plexo.Client.LocalProxy/Callback.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+using System.Web;
+using Goova.ElSwitch.Client.SDK;
+using Goova.ElSwitch.Client.SDK.Logging;
+
+namespace Goova.ElSwitch.Local.Proxy
+{
+    internal class Callback : ClientBase<ICallback>, ICallback //This will be loaded by reflection
+    {
+        public Callback()
+        {
+            int timeout = 120;
+            try
+            {
+                WebHttpBinding binding = new WebHttpBinding();
+                binding.OpenTimeout = TimeSpan.FromSeconds(timeout);
+                binding.CloseTimeout = TimeSpan.FromSeconds(timeout);
+                binding.SendTimeout = TimeSpan.FromSeconds(timeout);
+                binding.ReceiveTimeout = TimeSpan.FromSeconds(timeout);
+                if (Properties.Settings.Default.CallbackUrl.StartsWith("https"))
+                    binding.Security.Mode = WebHttpSecurityMode.Transport;
+
+                ServiceEndpoint svc = new ServiceEndpoint(ContractDescription.GetContract(typeof(ICallback)),
+                    binding, new EndpointAddress(Properties.Settings.Default.CallbackUrl));
+                WebHttpBehavior behavior = new WebHttpBehavior
+                {
+                    DefaultBodyStyle = WebMessageBodyStyle.Bare,
+                    DefaultOutgoingRequestFormat = WebMessageFormat.Json,
+                    DefaultOutgoingResponseFormat = WebMessageFormat.Json,
+                    HelpEnabled = true
+                };
+                svc.Behaviors.Add(behavior);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<ClientResponse> Instrument(IntrumentCallback instrument)
+        {
+            ClientResponse r=await Channel.Instrument(instrument);
+            r.Client = Properties.Settings.Default.ClientName;
+            return r;
+        }
+    }
+}
---
+++ Goova.Plexo.Client.LocalProxy/PaymentGateway.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using Goova.ElSwitch.Client.SDK;
+
+namespace Goova.ElSwitch.Local.Proxy
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PaymentGateway : IPaymentGateway
+    {
+        PaymentGatewayClient _cl = PaymentGatewayClientFactory.GetClient(Properties.Settings.Default.ClientName);
+
+        public Task<ServerResponse<string>> Authorize(Authorization authorization)
+        {
+            return _cl.Authorize(authorization);
+        }
+
+        public Task<ServerResponse<List<IssuerInfo>>> GetSupportedIssuers()
+        {
+            return _cl.GetSupportedIssuers();
+        }
+
+        public Task<ServerResponse<Transaction>> Purchase(PaymentRequest payment)
+        {
+            return _cl.Purchase(payment);
+        }
+
+        public Task<ServerResponse<Transaction>> Cancel(CancelRequest payment)
+        {
+            return _cl.Cancel(payment);
+        }
+    }
+}
