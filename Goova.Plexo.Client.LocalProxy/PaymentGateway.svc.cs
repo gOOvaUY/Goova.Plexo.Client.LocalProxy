92857aaa113d5dc522fcf4cc30ec7cda616ca294
Maximo Piva <maximo.piva@gmail.com>
2017-03-10 12:50:00 -0300

    Make Sure LocalProxy is only called from intranet

--- Goova.Plexo.Client.LocalProxy/PaymentGateway.svc.cs
+++ Goova.Plexo.Client.LocalProxy/PaymentGateway.svc.cs
@@ -1,31 +1,76 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Threading.Tasks;
+using System.Web;
 using Goova.Plexo.Client.SDK;
 
 namespace Goova.Plexo.Client.LocalProxy
 {
     public class PaymentGateway : IPaymentGateway
     {
-        PaymentGatewayClient _cl = PaymentGatewayClientFactory.GetClient(Local.Proxy.Properties.Settings.Default.ClientName);
+        private readonly PaymentGatewayClient _cl = PaymentGatewayClientFactory.GetClient(Local.Proxy.Properties.Settings.Default.ClientName);
 
-        public Task<ServerResponse<string>> Authorize(Authorization authorization)
+        public async Task<ServerResponse<string>> Authorize(Authorization authorization)
         {
-            return _cl.Authorize(authorization);
+            return await OnlyRunOnIntranet(() => _cl.Authorize(authorization));
         }
 
-        public Task<ServerResponse<List<IssuerInfo>>> GetSupportedIssuers()
+        public async Task<ServerResponse<List<IssuerInfo>>> GetSupportedIssuers()
         {
-            return _cl.GetSupportedIssuers();
+            return await OnlyRunOnIntranet(() => _cl.GetSupportedIssuers());
         }
 
-        public Task<ServerResponse<Transaction>> Purchase(PaymentRequest payment)
+        public async Task<ServerResponse<Transaction>> Purchase(PaymentRequest payment)
         {
-            return _cl.Purchase(payment);
+            return await OnlyRunOnIntranet(() => _cl.Purchase(payment));
         }
 
-        public Task<ServerResponse<Transaction>> Cancel(CancelRequest payment)
+        public async Task<ServerResponse<Transaction>> Cancel(CancelRequest payment)
         {
-            return _cl.Cancel(payment);
+            return await OnlyRunOnIntranet(() => _cl.Cancel(payment));
+        }
+
+        private async Task<ServerResponse<T>> OnlyRunOnIntranet<T>(Func<Task<ServerResponse<T>>> func)
+        {
+            string ip = null;
+            MessageProperties prop = OperationContext.Current?.IncomingMessageProperties;
+            if (prop != null)
+            {
+                if (prop.ContainsKey(RemoteEndpointMessageProperty.Name))
+                {
+                    RemoteEndpointMessageProperty endpoint = prop[RemoteEndpointMessageProperty.Name] as RemoteEndpointMessageProperty;
+                    if (endpoint != null)
+                        ip = endpoint.Address;
+                }
+            }
+            if (ip == null)
+                ip = HttpContext.Current.Request.UserHostAddress;
+            if (ip == null)
+                return new ServerResponse<T> {ErrorMessage = "Unable to get incoming ip", ResultCode = ResultCodes.SystemError};
+            if (IsIntranet(ip))
+                return await func();
+            return new ServerResponse<T> {ErrorMessage = "Not allowed to request from Internet", ResultCode = ResultCodes.Forbidden};
+        }
+
+        private bool IsIntranet(string ip)
+        {
+            if (ip.StartsWith("172."))
+            {
+                string[] parts = ip.Split('.');
+                int sec;
+                if (parts.Length > 1 && int.TryParse(parts[1], out sec))
+                {
+                    if (sec >= 16 && sec <= 32)
+                        return true;
+                }
+            }
+            if (ip.StartsWith("127.0.0.") || ip == "::1" || ip.StartsWith("10.") || ip.StartsWith("192.168."))
+                return true;
+            if (ip.Contains(":") && (ip.StartsWith("fc") || ip.StartsWith("fd")))
+                return true;
+            return false;
         }
     }
 }
